5fd1574e5ecf1bf86559da3c587e99364762b6ea
Kentaro321 <waddledee14@gmail.com>
2020-05-12 17:41:17 +0900

    implement fit/predict

--- model.py
+++ model.py
@@ -4,36 +4,72 @@ import torch
 import numpy as np
 from torch import nn
 
+from compression_networks import CompressionNetwork
+from estimation_networks import EstimationNetwork
 from gmm import GMM
-from compression_networks import CompressionNetworkArrhythmia
-from estimation_networks import EstimationNetworkArrhythmia
-
-eps = torch.autograd.Variable(torch.FloatTensor([1.e-8]), requires_grad=False)
+from utils import PyTorchDataset
 
 
 class DAGMM(nn.Module):
-    def __init__(self, compression_module, estimation_module, gmm_module,
-                 weight_1=0.1, weight_2=0.005):
+    def __init__(self, input_size, comp_hiddens, comp_activation,
+                 est_hiddens, est_activation, est_dropout_rate=0.5,
+                 n_epoch=100, batch_size=128, learning_rate=1e-4,
+                 lambda1=0.1, lambda2=0.005, device='cpu', random_seed=321):
         """
         Args:
-            compression_module (nn.Module): an autoencoder model that
-                implements at leat a function `self.encoder` to get the
-                encoding of a given input.
-            estimation_module (nn.Module): a FFNN model that estimates the
-                memebership of each input to a each mixture of a GMM.
-            gmm_module (nn.Module): a GMM model that implements its mixtures
-                as a list of Mixture classes. The GMM model should implement
-                the function `self._update_mixtures_parameters`.
-            weight_1 (float): a weight for the energy term in the loss function.
-            weight_2 (float): a weight for the penalty term in the loss function.
+            input_size: int
+                the dimension of inputs.
+            comp_hiddens: list of int
+                sizes of hidden layers of compression network
+                For example, if the sizes are [n1, n2],
+                structure of compression network is:
+                input_size -> n1 -> n2 -> n1 -> input_sizes
+            comp_activation: function
+                activation function of compression network
+            est_hiddens: list of int
+                sizes of hidden layers of estimation network.
+                The last element of this list is assigned as n_comp.
+                For example, if the sizes are [n1, n2],
+                structure of estimation network is:
+                input_size -> n1 -> n2 (= n_comp)
+            est_activation: function
+                activation function of estimation network
+            est_dropout_rate: float (optional)
+                dropout rate of estimation network applied during training
+                if 0 or None, dropout is not applied.
+            n_epoch: int (optional)
+                epoch size during training, used when fit() called.
+            batch_size: int (optional)
+                mini-batch size during training
+            learning_rate: float (optional)
+                learning rate during training, used when fit() called.
+            lambda1: float (optional)
+                a parameter of loss function (for energy term)
+            lambda2: float (optional)
+                a parameter of loss function
+                (for sum of diagonal elements of covariance)
+            device: str (optional)
+                'cpu' or 'cuda' ('cuda:x')
+            random_seed: int (optional)
+                random seed, used when fit() called.
         """
         super().__init__()
 
-        self.compressor = compression_module
-        self.estimator = estimation_module
-        self.gmm = gmm_module
-        self.weight_1 = weight_1
-        self.weight_2 = weight_2
+        dimension_embedding = comp_hiddens[-1] + 2
+        num_mixtures = est_hiddens[-1]
+        self.compressor = CompressionNetwork(input_size, comp_hiddens,
+                                             comp_activation)
+        self.estimator = EstimationNetwork(dimension_embedding, est_hiddens,
+                                           est_activation, est_dropout_rate)
+        self.gmm = GMM(num_mixtures, dimension_embedding)
+        self.n_epoch = n_epoch
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.lambda1 = lambda1
+        self.lambda2 = lambda2
+        self.device = torch.device(device)
+        self.random_seed = random_seed
+        self.eps = nn.Parameter(torch.Tensor([1e-6]), requires_grad=False)
 
     def forward(self, inputs):
         # Forward in the compression network.
@@ -41,43 +77,77 @@ class DAGMM(nn.Module):
         decoded = self.compressor.decode(encoded)
 
         # Preparing the input for the estimation network.
-        relative_ed = relative_euclidean_distance(inputs, decoded)
-        cosine_sim = cosine_similarity(inputs, decoded)
+        relative_ed = relative_euclidean_distance(inputs, decoded, self.eps)
+        cosine_sim = cosine_similarity(inputs, decoded, self.eps)
         # Adding a dimension to prepare for concatenation.
         relative_ed = relative_ed.view(-1, 1)
         cosine_sim = relative_ed.view(-1, 1)
-        latent_vectors = torch.cat([encoded, relative_ed, cosine_sim], dim=1)
-        # latent_vectors has shape [batch_size, dim_embedding + 2]
+        z = torch.cat([encoded, relative_ed, cosine_sim], dim=1)
+        # z has shape [batch_size, dim_embedding]
 
         # Updating the parameters of the mixture.
         if self.training:
-            mixtures_affiliations = self.estimator(latent_vectors)
-            # mixtures_affiliations has shape [batch_size, num_mixtures]
-            self.gmm._update_mixtures_parameters(latent_vectors,
-                                                 mixtures_affiliations)
+            gamma = self.estimator(z)
+            # gamma has shape [batch_size, num_mixtures]
+            self.gmm._update_mixtures_parameters(z, gamma)
         # Estimating the energy of the samples.
-        return self.gmm(latent_vectors)
+        return self.gmm(z)
 
     def calc_loss(self, inputs):
         reconstruction_loss = self.compressor.reconstruction_loss(inputs)
-        energy = self(inputs)
-        penalty = torch.sum(1. / torch.diagonal(self.gmm.Sigma, dim1=1, dim2=2))
-        return reconstruction_loss + self.weight_1 * energy + self.weight_2 * penalty
+        energy = torch.mean(self(inputs))
+        penalty = self.gmm.cov_diag_loss()
+        return reconstruction_loss + self.lambda1 * energy + self.lambda2 * penalty
+
+    def fit(self, x):
+        """Fit the DAGMM model according to the given data.
+
+        Args:
+            x: array-like, shape (n_samples, n_features)
+                Training data.
+        """
+        np.random.seed(self.random_seed)
+        torch.manual_seed(self.random_seed)
+
+        x = x.astype(np.float32)
+        dataset = PyTorchDataset(x)
+        dataloader = torch.utils.data.DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=True, drop_last=True)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+        self.to(self.device)
+        self.train()
+
+        for epoch in range(self.n_epoch):
+            for x in dataloader:
+                x = x.to(self.device)
+                optimizer.zero_grad()
+                loss = self.calc_loss(x)
+                loss.backward()
+                optimizer.step()
+
+            if (epoch + 1) % 100 == 0:
+                print(f"epoch {epoch+1}/{self.n_epoch} : loss = {loss.item():.3f}")
+
+    def predict(self, x):
+        """Calculate anormaly scores (sample energy) on samples in X.
+        Args:
+            x: array-like, shape (n_samples, n_features)
+                Data for which anomaly scores are calculated.
+                n_features must be equal to n_features of the fitted data.
+
+        Returns:
+            energies: array-like, shape (n_samples)
+                Calculated sample energies.
+        """
+        x = torch.from_numpy(x.astype(np.float32)).to(self.device)
+        if self.device == torch.device('cuda'):
+            return self(x).detach().cpu().numpy()
+        else:
+            return self(x).detach().numpy()
 
 
-class DAGMMArrhythmia(DAGMM):
-    def __init__(self):
-        compressor = CompressionNetworkArrhythmia()
-        estimator = EstimationNetworkArrhythmia()
-        # TODO: the GMM module should be part of the estimator....?
-        gmm = GMM(num_mixtures=2, dimension_embedding=4)
-
-        super().__init__(compression_module=compressor,
-                         estimation_module=estimator,
-                         gmm_module=gmm)
-
-
-def relative_euclidean_distance(x1, x2, eps=eps):
+def relative_euclidean_distance(x1, x2, eps):
     """x1 and x2 are assumed to be Variables or Tensors.
     They have shape [batch_size, dimension_embedding]"""
     num = torch.norm(x1 - x2, p=2, dim=1)  # dim [batch_size]
@@ -85,7 +155,7 @@ def relative_euclidean_distance(x1, x2, eps=eps):
     return num / torch.max(denom, eps)
 
 
-def cosine_similarity(x1, x2, eps=eps):
+def cosine_similarity(x1, x2, eps):
     """x1 and x2 are assumed to be Variables or Tensors.
     They have shape [batch_size, dimension_embedding]"""
     dot_prod = torch.sum(x1 * x2, dim=1)  # dim [batch_size]
